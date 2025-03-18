760979a39c2621e5a474c365f6a2ad5fb0ecd3f1
Cyrus Hiatt <chiatt@fargeo.com>
2025-03-18 11:39:49 -0700

    Adds search filter indicators #56

---
+++ afrc/src/afrc/Search/components/SearchFilterState.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import Button from "primevue/button";
+import { inject } from "vue";
+import type { Ref } from "vue";
+import type { SearchFilter } from "@/afrc/Search/types.ts";
+const searchFilters = inject("searchFilters") as Ref<SearchFilter[]>;
+</script>
+
+<template>
+    <div class="search-filters">
+        <div
+            v-for="(filter, index) in searchFilters"
+            :key="index"
+            class="search-filter"
+        >
+            <Button
+                :label="filter.name"
+                class="p-button"
+                severity="info"
+                raised
+                @click="filter.clear()"
+            />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.search-filter {
+    margin: 0px;
+    padding: 3px;
+}
+.search-filters {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+</style>
