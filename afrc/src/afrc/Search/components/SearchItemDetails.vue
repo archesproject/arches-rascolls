5da4a9f7c098202c02d0898b1d052961ea9e1cfb
Cyrus Hiatt <chiatt@fargeo.com>
2025-05-12 15:49:58 -0700

    Fix hard-coded analytic data value (#116)
    * Change hardcoded value to 'no data available' re #113
    * Remove zoom to item

--- afrc/src/afrc/Search/components/SearchItemDetails.vue
+++ afrc/src/afrc/Search/components/SearchItemDetails.vue
@@ -247,7 +247,7 @@ function zoomToSearchResult(resourceid: string, action: string) {
         <div class="resource-details">
             <div class="value-header">Analytic Data</div>
             <div class="value-entry">
-                <span class="resource-details-value">raman spectrum</span>
+                <span class="resource-details-value">No analytic data available</span>
             </div>
         </div>
         <div>
@@ -312,22 +312,6 @@ function zoomToSearchResult(resourceid: string, action: string) {
                     </div>
                 </div>
             </div>
-        <div v-if="hasGeom && showMap" class="zoom-to-item">
-                    <Button
-                        class="action-button"
-                        label="Zoom To Item"
-                        severity="secondary"
-                        text
-                        icon="pi pi-map-marker"
-                        size="large"
-                        @click="
-                            zoomToSearchResult(
-                                resultSelected,
-                                'zoom-and-select',
-                            )
-                        "
-                    />
-                </div>
     </div>
 </template>
 
