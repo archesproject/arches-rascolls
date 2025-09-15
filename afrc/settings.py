82bf4306bf8dfda5841636ea25d61b15728ee23c
Christopher Byrd <chrabyrd@gmail.com>
2025-09-15 15:35:56 -0700

    nit

--- afrc/settings.py
+++ afrc/settings.py
@@ -302,8 +302,6 @@ INSTALLED_APPS = (
     "pgtrigger",
 )
 
-FOO = True
-
 # Placing this last ensures any templates provided by Arches Applications
 # take precedence over core arches templates in arches/app/templates.
 INSTALLED_APPS += (
