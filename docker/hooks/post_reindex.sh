0fd40a19f6391a8860013ec01f9fa6fcd952a8f3
Aaron <124614+aarongundel@users.noreply.github.com>
2026-05-29 13:53:48 -0600

    updates post_reindex hook based on changes to arches search (#371)
    * updates post_reindex hook based on changes to arches search
    
    * updates readme
    
    * yank python 3.11
    
    * update pyproject.toml

--- docker/hooks/post_reindex.sh
+++ docker/hooks/post_reindex.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Rascolls post-reindex hook: arches-search db_index
 
-${WEB_ROOT}/ENV/bin/python manage.py db_index reindex_database
+${WEB_ROOT}/ENV/bin/python manage.py arches_search reindex_database -mp
