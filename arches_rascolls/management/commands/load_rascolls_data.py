a9d6a205c47c8906f09c39b77ed5be40d0314956
Cyrus Hiatt <chiatt@fargeo.com>
2026-08-25 11:13:53 -0700

    Remove mpx arg

--- arches_rascolls/management/commands/load_rascolls_data.py
+++ arches_rascolls/management/commands/load_rascolls_data.py
@@ -28,14 +28,6 @@ class Command(BaseCommand):
             default="tile-excel",
             help="Import format to use (default: tile-excel).",
         )
-        parser.add_argument(
-            "-mxp",
-            "--max-processes",
-            dest="mxp",
-            type=int,
-            default=5,
-            help="Number of parallel processes to use for the ETL import and search reindex (default: 5).",
-        )
 
     def handle(self, *args, **options):
         data_dir = Path(options["data_dir"]).resolve()
@@ -50,19 +42,14 @@ class Command(BaseCommand):
             raise CommandError(f"No *.xlsx files found in '{data_dir}'.")
 
         subcommand = IMPORTER_SUBCOMMANDS[options["format"]]
-        mxp = str(options["mxp"])
         for xlsx in xlsx_files:
-            self.stdout.write(
-                f"\n>>> etl {subcommand} -s {xlsx} -mp -mxp {mxp} --no-index"
-            )
-            call_command(
-                "etl", subcommand, "-s", str(xlsx), "-mp", "-mxp", mxp, "--no-index"
-            )
+            self.stdout.write(f"\n>>> etl {subcommand} -s {xlsx} -mp --no-index")
+            call_command("etl", subcommand, "-s", str(xlsx), "-mp", "--no-index")
 
         self.stdout.write("\n>>> resources calculate_descriptors")
         call_command("resources", "calculate_descriptors", "-y")
 
-        self.stdout.write(f"\n>>> arches_search reindex_database -mp -mxp {mxp}")
-        call_command("arches_search", "reindex_database", "-mp", "-mxp", mxp)
+        self.stdout.write("\n>>> arches_search reindex_database -mp")
+        call_command("arches_search", "reindex_database", "-mp")
 
         self.stdout.write(self.style.SUCCESS("\nDone."))
